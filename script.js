49d5b84aa0f36067cc52bdf5b41a798e5975650b
Chaxzer <110409740+Chaxzer@users.noreply.github.com>
2024-02-02 04:21:28 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+//you can us 'finaly' with try catch it will always runin the case
+
+try{
+  // document.body.appendChild(document.createElement('h3')).innerText="jai ram ji ki" // if you want it to run
+
+ console.log(ram) //intentional error
+}
+catch(error){
+  console.log("ram nahi aai")
+ document.body.appendChild(document.createElement('h3')).innerText="har har mahadev"
+  
+}
